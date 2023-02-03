1270b989a6c7707fbdb32873405eff5b6b4d337c
nchinling <nchinling@gmail.com>
2023-02-03 19:04:58 +0800

    First commit

--- src/myapp/Main.java
+++ src/myapp/Main.java
@@ -9,6 +9,7 @@ public class Main {
         public static void main(String[] args){
             System.out.printf("Hello, World. I am Chin Ling\n");
             System.out.println("Hello, there\n");
+            //This is fun.
         }
 
 }
